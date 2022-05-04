ab61dfa8a81927fab01157a70aa2f55fdca33c0d
ImDaBestMan <asifarham1704@gmail.com>
2022-05-04 09:59:09 -0700

    I made some changes

--- Wordle.java
+++ Wordle.java
@@ -22,7 +22,7 @@ class Wordle
         this.playerGuesses = new String[6];
     }
     
-    void guessWord()
+    void guessWord() throws FileNotFoundException
     {
         Scanner s = new Scanner(System.in);
         
@@ -30,13 +30,29 @@ class Wordle
         
         String guess = s.nextLine();
         
-        playerGuesses[0] = guess;     
+        while (guess.length() != 5 || isInList(guess) == false)
+        {
+            if (guess.length() != 5)
+            {
+                System.out.println("5 letters please!");
+            }
+            if (isInList(guess) == false)
+            {
+                System.out.println("What is that word?");
+            }
+            System.out.println("Guess again.");
+            
+            guess = s.nextLine();
+        }
+        
+        playerGuesses[6 - guesses] = guess;
+        guesses--;
     }
 
     static void wordList() throws FileNotFoundException
     {
         int number = 0;
-        File f = new File("wordlist.txt");
+        File f = new File("wordlelist.txt");
         Scanner s = new Scanner(f);
 
         while(s.hasNextLine())
@@ -72,12 +88,12 @@ class Wordle
     {
         File f = new File("wordlist.txt");
         Scanner s = new Scanner(f);
-
+        
         while(s.hasNextLine())
         {
             String data = s.next();
 
-            if(data.equals(word))
+            if(data.equals(word.toUpperCase()))
             {
                 return true;
             }
@@ -91,7 +107,7 @@ class Wordle
         int randIndex = r.nextInt(5757);
         int number = 0;
 
-        File f = new File("wordlist.txt");
+        File f = new File("wordlelist.txt");
         Scanner s = new Scanner(f);
 
         while(s.hasNextLine())
@@ -109,5 +125,14 @@ class Wordle
         s.close();
 
         return null;
+    }
+    
+    void play() throws FileNotFoundException
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            guessWord();
+            System.out.println(playerGuesses[i]);
+        }
     }
 }
